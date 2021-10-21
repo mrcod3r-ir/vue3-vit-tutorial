a8aa40d6878757c7f240bc1fdb0897b77ce5a0d6
mrcod3r <mrcod3r.ir@gmail.com>
2021-10-21 17:22:46 +0330

    init & complete search query

---
+++ README.md
@@ -0,0 +1,9 @@
+# vite vue3 app
+
+this tutorial is from - [learnvue.co](https://learnvue.co/2020/12/setting-up-your-first-vue3-project-vue-3-0-release/#started)
+
+## installation
+
+```sh
+yarn install
+```
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,31 @@
+<template>
+	<h1>{{ msg }}</h1>
+	<h2>Filter LearnVue Articles</h2>
+	<input type="text" placeholder="Filter Search" v-model="query" />
+	<br />
+	<br />
+	<button @click="reset">Reset</button>
+	<br />
+	<br />
+	<search-results :query="query" />
+	<span style="margin-bottom: 40px; display: block" />
+</template>
+
+<script>
+	import { ref } from "vue"
+	import SearchResults from "./SearchResults.vue"
+	export default {
+		name: "HelloWorld",
+		components: { SearchResults },
+		props: {
+			msg: String
+		},
+		setup() {
+			const query = ref("")
+			const reset = (evt) => {
+				query.value = "" //clear the query
+			}
+			return { reset, query }
+		}
+	}
+</script>
